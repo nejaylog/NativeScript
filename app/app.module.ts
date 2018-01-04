c3bc6d66bc2feb01977f5f8d8bcfd793fb257000
Hunchul Jung <nejaylog@gmail.com>
2018-01-04 17:04:30 +0900

    Navigation Part 1

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,18 +1,14 @@
-import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
-import { NativeScriptModule } from "nativescript-angular/nativescript.module";
-import { AppRoutingModule } from "./app.routing";
-import { AppComponent } from "./app.component";
-import { NativeScriptHttpModule } from "nativescript-angular/http";
-import { MenuComponent } from './menu/menu.component';
+import {NgModule, NO_ERRORS_SCHEMA} from "@angular/core";
+import {NativeScriptModule} from "nativescript-angular/nativescript.module";
+import {AppRoutingModule} from "./app.routing";
+import {AppComponent} from "./app.component";
+import {NativeScriptHttpModule} from "nativescript-angular/http";
+import {MenuComponent} from './menu/menu.component';
+import {DishdetailComponent} from './dishdetail/dishdetail.component';
 
-import { DishService } from './services/dish.service';
-import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
-
-import { baseURL } from './shared/baseurl';
-
-// import { ItemService } from "./item/item.service";
-// import { ItemsComponent } from "./item/items.component";
-// import { ItemDetailComponent } from "./item/item-detail.component";
+import {DishService} from './services/dish.service';
+import {ProcessHTTPMsgService} from './services/process-httpmsg.service';
+import {baseURL} from './shared/baseurl';
 
 // Uncomment and add to NgModule imports if you need to use two-way binding
 // import { NativeScriptFormsModule } from "nativescript-angular/forms";
@@ -31,7 +27,8 @@ import { baseURL } from './shared/baseurl';
     ],
     declarations: [
         AppComponent,
-        MenuComponent
+        MenuComponent,
+        DishdetailComponent
     ],
     providers: [
         {provide: 'BaseURL', useValue: baseURL},
@@ -43,6 +40,7 @@ import { baseURL } from './shared/baseurl';
     ]
 })
 /*
-Pass your application module to the bootstrapModule function located in main.ts to start your app
-*/
-export class AppModule { }
+ Pass your application module to the bootstrapModule function located in main.ts to start your app
+ */
+export class AppModule {
+}
