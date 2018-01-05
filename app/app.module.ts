7bb46d713ff7c8341551dca3c86febe639369627
Hunchul Jung <nejaylog@gmail.com>
2018-01-05 11:09:13 +0900

    Navigation Part 2

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,11 +5,14 @@ import {NativeScriptHttpModule} from "nativescript-angular/http";
 import {NativeScriptUISideDrawerModule} from 'nativescript-telerik-ui/sidedrawer/angular';
 
 import {AppComponent} from "./app.component";
+import {HomeComponent} from './home/home.component';
 import {MenuComponent} from './menu/menu.component';
 import {DishdetailComponent} from './dishdetail/dishdetail.component';
 import {DrawerComponent} from './shared/drawer/drawer.component';
 
 import {DishService} from './services/dish.service';
+import {PromotionService} from "./services/promotion.service";
+import {LeaderService} from "./services/leader.service";
 import {ProcessHTTPMsgService} from './services/process-httpmsg.service';
 import {baseURL} from './shared/baseurl';
 
@@ -31,6 +34,7 @@ import {baseURL} from './shared/baseurl';
     ],
     declarations: [
         AppComponent,
+        HomeComponent,
         MenuComponent,
         DishdetailComponent,
         DrawerComponent
@@ -38,6 +42,8 @@ import {baseURL} from './shared/baseurl';
     providers: [
         {provide: 'BaseURL', useValue: baseURL},
         DishService,
+        PromotionService,
+        LeaderService,
         ProcessHTTPMsgService
     ],
     schemas: [
--- app/app.routing.ts
+++ app/app.routing.ts
@@ -1,12 +1,14 @@
-import { NgModule } from "@angular/core";
-import { NativeScriptRouterModule } from "nativescript-angular/router";
-import { Routes } from "@angular/router";
-import { MenuComponent } from './menu/menu.component';
+import {NgModule} from "@angular/core";
+import {NativeScriptRouterModule} from "nativescript-angular/router";
+import {Routes} from "@angular/router";
+import {MenuComponent} from './menu/menu.component';
+import {HomeComponent} from './home/home.component';
 import {DishdetailComponent} from "./dishdetail/dishdetail.component";
 
 const routes: Routes = [
-    { path: "", redirectTo: "/menu", pathMatch: "full" },
-    { path: "menu", component: MenuComponent },
+    {path: "", redirectTo: "/home", pathMatch: "full"},
+    {path: "home", component: HomeComponent},
+    {path: "menu", component: MenuComponent},
     {path: 'dishdetail/:id', component: DishdetailComponent}
 ];
 
@@ -14,4 +16,5 @@ const routes: Routes = [
     imports: [NativeScriptRouterModule.forRoot(routes)],
     exports: [NativeScriptRouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
