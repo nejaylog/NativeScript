f6d2e4d693a26c2bd1fc7530a556de798588f51c
Hunchul Jung <nejaylog@gmail.com>
2018-01-10 14:54:56 +0900

    Modal Dialog

--- app/app.module.ts
+++ app/app.module.ts
@@ -16,6 +16,7 @@ import {ContactComponent} from './contact/contact.component';
 import {DrawerComponent} from './shared/drawer/drawer.component';
 import {FavoritesComponent} from './favorites/favorites.component';
 import {ReservationComponent} from './reservation/reservation.component';
+import {ReservationModalComponent} from './reservationmodal/reservationmodal.component';
 
 import {DishService} from './services/dish.service';
 import {PromotionService} from "./services/promotion.service";
@@ -55,8 +56,10 @@ import {baseURL} from './shared/baseurl';
         ContactComponent,
         DrawerComponent,
         FavoritesComponent,
-        ReservationComponent
+        ReservationComponent,
+        ReservationModalComponent
     ],
+    entryComponents: [ReservationModalComponent],
     providers: [
         {provide: 'BaseURL', useValue: baseURL},
         DishService,
